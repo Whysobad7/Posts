6e31837f791d43e19e101a5c7e9ae0f14a2cb13e
Adilet Kasimov <kasimoff17@gmail.com>
2023-10-30 22:53:53 +0300

    yes
    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout'
 import PostPage from "./pages/PostPage";
 import Main from "./Main";
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React from 'react';
 import MyButton from './UI/button/MyButton';
 import { useNavigate } from 'react-router-dom'
 
